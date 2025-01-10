5eff91117e432befb84c3c58dc67c8ac975e22d2
TusharBR <tusharbaddi2002@gmail.com>
2025-01-10 16:08:44 +0530

    add nav & accordian

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import '../index.css'
+
+export default function Navbar() {
+  return (
+    <>
+    <li className="mainli">
+
+        <li><ol className="ollogo">Logo</ol></li>
+        <li><ol className="ollist">Home</ol>
+        <ol className="ollist">About us</ol>
+        <ol className="ollist">Notification</ol>
+        <ol className="ollist">Logout</ol></li>
+        
+    </li>
+    </>
+  )
+}
