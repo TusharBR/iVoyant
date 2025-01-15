ea7d721b62887c7e49d2e7aceb8c7adc52a81c30
TusharBR <tusharbaddi2002@gmail.com>
2025-01-15 14:48:21 +0530

    propsdrilling

--- src/components/Firstchild.tsx
+++ src/components/Firstchild.tsx
@@ -7,16 +7,13 @@ type Props= {
   }
 
 
-    const Profile = ({ name, age,city,company }: Props) => (
+    const Firstchild = ({ name, age,city,company }: Props) => (
         <div>
-          
-          
           <h3 >I am from <span>Firstchild</span> and my name is <span>{name}</span></h3>
            <Secondchild  age={age} city={city} company={company}/>
-           
         </div>
       );   
   
 
 
-export default Profile
+export default Firstchild
