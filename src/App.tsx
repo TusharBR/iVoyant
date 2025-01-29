97557afc3b3ad8a91ff4ea5fbeb10b71a0c540cc
TusharBR <tusharbaddi2002@gmail.com>
2025-01-29 17:36:38 +0530

    mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
+type props={
+  initial:number
+}
+function App({initial}:props) {
+  const [count, setCount] = useState(initial)
+
+
+  useEffect(()=>
+  {
+    console.log('component did mount');
+    setTimeout(()=>{
+      setCount(count+1)
+    },1000)
+    return ()=>
+    {
+      console.log('component will unmount',count)
+    }
+  },[count])
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   
+      <h1>Counter app - {count}</h1>
+    
+      <button >Click me</button>       
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <App initial={10}/>
   </StrictMode>,
 )
