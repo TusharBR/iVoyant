849033c70879229879ad29045d8046c0954a50ba
TusharBR <tusharbaddi2002@gmail.com>
2025-02-24 14:14:02 +0530

    optimised:qusetion1

--- src/assets/Api.jsx
+++ src/assets/Api.jsx
@@ -1,45 +1,39 @@
 import { useEffect, useState } from "react"
-import "../App.css"
-
+import { nanoid } from "nanoid";
 const Api = () => {
     
     let [abc,setabc]=useState();
     let [cond,setcond]=useState(false);
     let [iniidx,setiniidx]=useState();
     useEffect(() => {
-        const first=fetch("https://randomuser.me/api/?results=20")
+        fetch("https://randomuser.me/api/?results=20")
         .then((data)=>data.json())
-        .then((data)=>
-            {
-                console.log(data.results);
-                setabc(data.results);
-                console.log(abc,"hii i am abc")
-    })
+        .then((data)=>setabc(data.results.map((cv)=>{
+            return cv.email
+        })))
     }, [])
 
-    // abc?.sort((a, b) => a.email.localeCompare(b.email));
-    const emails=abc?.map((cv)=>{
-        return cv.email
-    })
-    abc=emails?.sort()
+
+    abc?.sort()
     const overlining = (idx) => {
         setcond(!cond)
         setiniidx(idx)
-        // const element = document.getElementById(idx).firstElementChild;
-      
-        // if (element.style.textDecoration === "line-through") {
-        //   element.style.textDecoration = "none";
-        // } else {
-        //   element.style.textDecoration = "line-through";
-        // }
       };
+      const deleteobj=(idx1)=>
+      {
+     abc= abc.filter((cv)=>cv!==idx1)
+       setabc(abc)
+       setcond(false)       
+      }
     return (
         <>
     {abc?.map((cv,idx)=>{
         return(
-            <h1 style={{border:"2px solid red "}} id={idx} key={idx}>
-                <span>{(cond && idx==iniidx) ?<s>{cv}</s>:<span>{cv}</span>}</span>
-            <input type="checkbox" onClick={()=>overlining(idx)}></input>
+            <h1 style={{border:"2px solid red ",display:"flex",justifyContent:'space-between',alignItems:"center"}} id={idx} key={idx}>
+                <span >{idx}) {(cond && idx==iniidx) ?<s>{cv}</s>:cv}</span>
+                 <button onClick={()=>deleteobj(cv)}>Delete</button>
+            <input type="checkbox" checked={(cond && idx==iniidx)} className="inp" onClick={()=>overlining(idx)}></input>
+           
             </h1>
         )
     })}
