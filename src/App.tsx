eca986a0b992f8f319a35fa58ec360f68130d08e
TusharBR <tusharbaddi2002@gmail.com>
2025-02-27 14:31:42 +0530

    mock1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import { useState,useEffect} from 'react'
-
-import './App.css'
-type props={
-  initial:number
-}
-function App({initial}:props) {
-  const [count, setCount] = useState(initial)
 
 
-  useEffect(()=>
-  {
-    console.log('component did mount');
-    setTimeout(()=>{
-      setCount(count+1)
-    },1000)
-    return ()=>
-    {
-      console.log('component will unmount',count)
-    }
-  },[count])
-
-
-
-
+const App = () => {
   return (
-    <>
-   
-      <h1>Counter app - {count}</h1>
-    
-      <button >Click me</button>       
-    </>
+    <div>App</div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App initial={10}/>
+    <App/>
   </StrictMode>,
 )
