eb896a39778e13f1ce6c663d08f913cf58200e9c
TusharBR <tusharbaddi2002@gmail.com>
2025-03-15 03:44:13 +0530

    bug:buttonscssfixed

--- src/components/atoms/Random.tsx
+++ src/components/atoms/Random.tsx
@@ -4,10 +4,10 @@ export const createUniqueRandomGenerator = (min: number, max: number) => {
     const availableNumbers = Array.from({ length: max - min + 1 }, (_, i) => i + min);
     
     return () => {
-      if (availableNumbers.length === 0) return null; // No more unique numbers left
+      if (availableNumbers.length === 0) return null; 
       
       const randomIndex = Math.floor(Math.random() * availableNumbers.length);
-      return availableNumbers.splice(randomIndex, 1)[0]; // Remove and return the number
+      return availableNumbers.splice(randomIndex, 1)[0]; 
     };
   };
   
