d1f8d89deab4978603f937326789dcfb5b942c56
TusharBR <tusharbaddi2002@gmail.com>
2025-03-15 04:06:34 +0530

    storybook:final

--- stories/Button.jsx
+++ stories/Button.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-
 import './button.css';
 
-/** Primary UI component for user interaction */
 export const Button = ({
   primary = false,
   backgroundColor = null,
@@ -26,14 +24,9 @@ export const Button = ({
 };
 
 Button.propTypes = {
-  /** Is this the principal call to action on the page? */
   primary: PropTypes.bool,
-  /** What background color to use */
   backgroundColor: PropTypes.string,
-  /** How large should the button be? */
   size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /** Button contents */
   label: PropTypes.string.isRequired,
-  /** Optional click handler */
   onClick: PropTypes.func,
 };
